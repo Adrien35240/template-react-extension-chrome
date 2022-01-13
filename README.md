b03e9d5765593bce8b1876272de3393bc971e45d
Adrien35240 <adriendev35240@gmail.com>
2022-01-13 13:41:05 +0100

    docs: update readme

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
+## Une application simple qui renvoie le titre de la page visité lorsque l'on clique sur l'icone.
 ### Installation sur chrome en mode developpeur.
 ```
 npm i && npm run build
 ```
-télécharger le dossier build avec le manager d'extensions chrome
+UpLoader le dossier build crée avec le manager d'extensions chrome
 
--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
+/* eslint-disable no-undef */
+import {useState,useEffect} from 'react'
 import './App.css';
 
 function App() {
-  
+  // declaration d'un hook
+const [titleUrl,setTitleUrl] = useState('')
+
+  // s'execute à l'ouverture de l'extension
+  useEffect(()=>{
+    init() 
+    // ajoute un ecouter de message et renvoi la valeur dans un hook
+  chrome.runtime.onMessage.addListener((request)=>{
+  setTitleUrl(request.title)
+})})
+
+  // envoi le contentScript.js dans la page ciblé par tabId 
+   async function init() {
+    const tab = await getCurrentTab();
+    chrome.scripting.executeScript({ target: { tabId: tab.id }, files: ['js/contentScript.js'] });
+  }
+    // Recupere les infos de l'onglet actif
+    async function getCurrentTab() {
+    const queryOptions = { active: true, currentWindow: true };
+    const [tab] = await chrome.tabs.query(queryOptions);
+    return tab;
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        Hello World
+        Hello World, vous êtes sur le site {titleUrl}
       </header>
     </div>
   );
 }
-
+  
 export default App;
