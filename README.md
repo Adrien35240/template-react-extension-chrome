9cbd740fcddb1239324181b7ed938562d2c5c88a
Adrien35240 <adriendev35240@gmail.com>
2022-01-13 10:23:21 +0100

    docs:update readme

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
+### Installation sur chrome en mode developpeur.
+```
+npm i && npm run build
+```
+télécharger le dossier build avec le manager d'extensions chrome
 
--- src/App.js
+++ src/App.js
@@ -2,14 +2,6 @@ import './App.css';
 
 function App() {
   
-//Create a permanent link with js/background.js
-/*  function createListener() {
-    chrome.runtime.onConnect.addListener((port) => {
-      port.onMessage.addListener(callback);
-    });
-  }*/
-
-
   return (
     <div className="App">
       <header className="App-header">
